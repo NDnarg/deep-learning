622ac3c954bbc4e62d8c9a6d8e2c356c60282aa5
NDnarg <68454474+NDnarg@users.noreply.github.com>
2020-08-05 17:30:51 +0800

    Add files via upload

---
+++ deep neural network/not finished dnn.py
@@ -0,0 +1,95 @@
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+from dnn_utils import sigmoid, sigmoid_backward, relu, relu_backward
+import lr_utils
+
+np.random.seed(1)
+
+
+def initialize_parameters(n_x, n_h, n_y):
+
+    W1 = np.random.randn(n_h, n_x) * 0.01
+    b1 = np.zeros((n_h, 1))
+    W2 = np.random.randn(n_y, n_h) * 0.01
+    b2 = np.zeros((n_y, 1))
+
+    assert (W1.shape == (n_h, n_x))
+    assert (b1.shape == (n_h, 1))
+    assert (W2.shape == (n_y, n_h))
+    assert (b2.shape == (n_y, 1))
+
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+
+    return parameters
+
+
+def initialize_parameters_deep(layers_dims):
+
+    np.random.seed(3)
+    parameters = {}
+    L = len(layers_dims)
+
+    for l in range(1, L):
+        parameters["W" + str(l)] = np.random.randn(layers_dims[l], layers_dims[l - 1]) / np.sqrt(layers_dims[l - 1])
+    parameters["b" + str(l)] = np.zeros((layers_dims[l], 1))
+
+    assert (parameters["W" + str(l)].shape == (layers_dims[l], layers_dims[l - 1]))
+    assert (parameters["b" + str(l)].shape == (layers_dims[l], 1))
+
+
+    return parameters
+
+
+def linear_forward(A, W, b):
+    Z = np.dot(W, A) + b
+    assert (Z.shape == (W.shape[0], A.shape[1]))
+    cache = (A, W, b)
+
+    return Z, cache
+
+
+def linear_activation_forward(A_prev, W, b, activation):
+
+    if activation == "sigmoid":
+        Z, linear_cache = linear_forward(A_prev, W, b)
+        A, activation_cache = sigmoid(Z)
+    elif activation == "relu":
+        Z, linear_cache = linear_forward(A_prev, W, b)
+        A, activation_cache = relu(Z)
+
+    assert (A.shape == (W.shape[0], A_prev.shape[1]))
+    cache = (linear_cache, activation_cache)
+
+    return A, cache
+
+
+def L_model_forward(X, parameters):
+    caches = []
+    A = X
+    L = len(parameters) // 2
+    for l in range(1, L):
+        A_prev = A
+        A, cache = linear_activation_forward(A_prev, parameters['W' + str(l)], parameters['b' + str(l)], "relu")
+        caches.append(cache)
+
+    AL, cache = linear_activation_forward(A, parameters['W' + str(L)], parameters['b' + str(L)], "sigmoid")
+    caches.append(cache)
+
+    assert (AL.shape == (1, X.shape[1]))
+
+    return AL, caches
+
+
+def compute_cost(AL, Y):
+
+    m = Y.shape[1]
+    cost = -np.sum(np.multiply(np.log(AL), Y) + np.multiply(np.log(1 - AL), 1 - Y)) / m
+
+    cost = np.squeeze(cost)
+    assert (cost.shape == ())
+
+    return cost

a2e8c1680c2dc1a514df1c97757a26106d139fa3
NDnarg <68454474+NDnarg@users.noreply.github.com>
2020-08-14 18:57:38 +0800

    Add files via upload

---
+++ gradient descent optimization/gd.py
@@ -0,0 +1,118 @@
+import numpy as np
+
+def update_parameters_with_gd(parameters, grads, learning_rate):
+    L = len(parameters) // 2  # 神经网络的层数
+    # 更新每个参数
+    for l in range(L):
+        parameters["W" + str(l + 1)] = parameters["W" + str(l + 1)] - learning_rate * grads["dW" + str(l + 1)]
+        parameters["b" + str(l + 1)] = parameters["b" + str(l + 1)] - learning_rate * grads["db" + str(l + 1)]
+
+    return parameters
+
+
+def random_mini_batches(X, Y, mini_batch_size=64, seed=0):
+
+    np.random.seed(seed)  # 指定随机种子
+    m = X.shape[1]
+    mini_batches = []
+
+    # 第一步：打乱顺序
+    permutation = list(np.random.permutation(m))  # 它会返回一个长度为m的随机数组，且里面的数是0到m-1
+    shuffled_X = X[:, permutation]  # 将每一列的数据按permutation的顺序来重新排列。
+    shuffled_Y = Y[:, permutation].reshape((1, m))
+
+    # 第二步，分割
+    num_complete_minibatches = np.math.floor(m / mini_batch_size)
+    for k in range(0, num_complete_minibatches):
+        mini_batch_X = shuffled_X[:, k * mini_batch_size:(k + 1) * mini_batch_size]
+        mini_batch_Y = shuffled_Y[:, k * mini_batch_size:(k + 1) * mini_batch_size]
+
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+
+    # 如果训练集的大小刚好是mini_batch_size的整数倍，那么这里已经处理完了
+    # 如果训练集的大小不是mini_batch_size的整数倍，那么最后肯定会剩下一些，我们要把它处理了
+    if m % mini_batch_size != 0:
+        # 获取最后剩余的部分
+        mini_batch_X = shuffled_X[:, mini_batch_size * num_complete_minibatches:]
+        mini_batch_Y = shuffled_Y[:, mini_batch_size * num_complete_minibatches:]
+
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+
+    return mini_batches
+
+
+def initialize_velocity(parameters):
+
+    L = len(parameters) // 2  # 神经网络的层数
+    v = {}
+
+    for l in range(L):
+        v["dW" + str(l + 1)] = np.zeros_like(parameters["W" + str(l + 1)])
+        v["db" + str(l + 1)] = np.zeros_like(parameters["b" + str(l + 1)])
+
+    return v
+
+
+def update_parameters_with_momentun(parameters, grads, v, beta, learning_rate):
+
+    L = len(parameters) // 2
+    for l in range(L):
+        # 计算速度
+        v["dW" + str(l + 1)] = beta * v["dW" + str(l + 1)] + (1 - beta) * grads["dW" + str(l + 1)]
+        v["db" + str(l + 1)] = beta * v["db" + str(l + 1)] + (1 - beta) * grads["db" + str(l + 1)]
+
+        # 更新参数
+        parameters["W" + str(l + 1)] = parameters["W" + str(l + 1)] - learning_rate * v["dW" + str(l + 1)]
+        parameters["b" + str(l + 1)] = parameters["b" + str(l + 1)] - learning_rate * v["db" + str(l + 1)]
+
+    return parameters, v
+
+
+def initialize_adam(parameters):
+
+    L = len(parameters) // 2
+    v = {}
+    s = {}
+
+    for l in range(L):
+        v["dW" + str(l + 1)] = np.zeros_like(parameters["W" + str(l + 1)])
+        v["db" + str(l + 1)] = np.zeros_like(parameters["b" + str(l + 1)])
+
+        s["dW" + str(l + 1)] = np.zeros_like(parameters["W" + str(l + 1)])
+        s["db" + str(l + 1)] = np.zeros_like(parameters["b" + str(l + 1)])
+
+    return (v, s)
+
+
+def update_parameters_with_adam(parameters, grads, v, s, t, learning_rate=0.01, beta1=0.9, beta2=0.999, epsilon=1e-8):
+
+    L = len(parameters) // 2
+    v_corrected = {}  # 偏差修正后的值
+    s_corrected = {}  # 偏差修正后的值
+
+    for l in range(L):
+        # 梯度的移动平均值,输入："v , grads , beta1",输出：" v "
+        v["dW" + str(l + 1)] = beta1 * v["dW" + str(l + 1)] + (1 - beta1) * grads["dW" + str(l + 1)]
+        v["db" + str(l + 1)] = beta1 * v["db" + str(l + 1)] + (1 - beta1) * grads["db" + str(l + 1)]
+
+        # 计算第一阶段的偏差修正后的估计值，输入"v , beta1 , t" , 输出："v_corrected"
+        v_corrected["dW" + str(l + 1)] = v["dW" + str(l + 1)] / (1 - np.power(beta1, t))
+        v_corrected["db" + str(l + 1)] = v["db" + str(l + 1)] / (1 - np.power(beta1, t))
+
+        # 计算平方梯度的移动平均值，输入："s, grads , beta2"，输出："s"
+        s["dW" + str(l + 1)] = beta2 * s["dW" + str(l + 1)] + (1 - beta2) * np.square(grads["dW" + str(l + 1)])
+        s["db" + str(l + 1)] = beta2 * s["db" + str(l + 1)] + (1 - beta2) * np.square(grads["db" + str(l + 1)])
+
+        # 计算第二阶段的偏差修正后的估计值，输入："s , beta2 , t"，输出："s_corrected"
+        s_corrected["dW" + str(l + 1)] = s["dW" + str(l + 1)] / (1 - np.power(beta2, t))
+        s_corrected["db" + str(l + 1)] = s["db" + str(l + 1)] / (1 - np.power(beta2, t))
+
+        # 更新参数，输入: "parameters, learning_rate, v_corrected, s_corrected, epsilon". 输出: "parameters".
+        parameters["W" + str(l + 1)] = parameters["W" + str(l + 1)] - learning_rate * (
+                    v_corrected["dW" + str(l + 1)] / np.sqrt(s_corrected["dW" + str(l + 1)] + epsilon))
+        parameters["b" + str(l + 1)] = parameters["b" + str(l + 1)] - learning_rate * (
+                    v_corrected["db" + str(l + 1)] / np.sqrt(s_corrected["db" + str(l + 1)] + epsilon))
+
+    return (parameters, v, s)
